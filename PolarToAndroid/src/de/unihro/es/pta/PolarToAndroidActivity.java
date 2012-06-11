30153752d7f5fbd30f3a6e75ac144a6e904b9793
andersenhansandersen@gmail.com <andersenhansandersen@gmail.com@91149799-462f-4b61-dd74-96b598605db3>
2012-06-11 12:47:52 +0000

    First Bluetooth settings.

--- PolarToAndroid/src/de/unihro/es/pta/PolarToAndroidActivity.java
+++ PolarToAndroid/src/de/unihro/es/pta/PolarToAndroidActivity.java
@@ -1,13 +1,40 @@
 package de.unihro.es.pta;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PolarToAndroidActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	private static final int REQUEST_ENABLE_BT = 1; 
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		TextView vText = (TextView) findViewById(R.id.bla);
+
+		//	Init Bluetooth
+		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (mBluetoothAdapter == null) {
+			vText.setText("Bluetooth is not supported.");
+		}else{
+
+			vText.setText("Everything's cool.");
+			
+			//	Enable Bluetooth if neccessary
+			if (!mBluetoothAdapter.isEnabled()) {
+				Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+				startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+			}
+
+			
+			
+			
+		}
+	}
 }
