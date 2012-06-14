b5596f30c24dea93175c04f17827a2f556090f4c
maikklasen@googlemail.com <maikklasen@googlemail.com@91149799-462f-4b61-dd74-96b598605db3>
2012-06-14 18:26:13 +0000

    Funktioniert :D :D :D

--- PolarToAndroid/src/de/unihro/es/pta/PolarToAndroidActivity.java
+++ PolarToAndroid/src/de/unihro/es/pta/PolarToAndroidActivity.java
@@ -9,7 +9,6 @@ import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -18,6 +17,7 @@ import android.widget.Toast;
 public class PolarToAndroidActivity extends Activity {
 
 	private static final int REQUEST_ENABLE_BT = 1; 
+	private static final UUID Polar_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
 	/** Called when the activity is first created. */
 	@Override
@@ -57,9 +57,8 @@ public class PolarToAndroidActivity extends Activity {
 					break;
 				}
 				BluetoothSocket socket = null;
-				UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 				try {
-					socket = device.createRfcommSocketToServiceRecord(MY_UUID);
+					socket = device.createRfcommSocketToServiceRecord(Polar_UUID);
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -72,31 +71,36 @@ public class PolarToAndroidActivity extends Activity {
 					e.printStackTrace();
 				}
 
-				try {
-					InputStream a = socket.getInputStream();
-					byte[] buffer = new byte[1024];
-					a.read(buffer);
-					boolean heartrateValid = false;
-					int heartRate = 0;
-					out="";
-					for (int i = 0; i < buffer.length - 8; i++) {  
-						heartrateValid = packetValid(buffer,i);
-						if(heartrateValid){
-							heartRate = buffer[i + 5] & 0xFF;
-							break;
-						} 
-					out = Integer.toString(heartRate);
+
+				int heartRate = 0;
+				out="";
+				InputStream a = null;
+				byte[] buffer = new byte[16];
+				for(int j = 0; j < 20; j++){
+					try {
+						a = socket.getInputStream();
+					} catch (IOException e2) {
+						// TODO Auto-generated catch block
+						e2.printStackTrace();
 					}
-					
 
+					try {
+						a.read(buffer);
+					} catch (IOException e2) {
+						// TODO Auto-generated catch block
+						e2.printStackTrace();
+					}
 
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
+					for (int i = 0; i < buffer.length - 8; i++) { 
+
+						if (packetValid(buffer, i)) {
+							heartRate = buffer[i + 5] & 0xFF;
+							out = Integer.toString(heartRate);
+							//vText.setText(out);
+							break;
+						}
+					}
 				}
-
-
-
 				try {
 					socket.close();
 				} catch (IOException e) {
@@ -106,17 +110,33 @@ public class PolarToAndroidActivity extends Activity {
 
 			}
 
-			vText.setText(out);
-
 		}
+		vText.setText(out);
+		Toast.makeText(this, out, Toast.LENGTH_LONG).show();
+
 
 	}
 
-	private boolean packetValid(byte[] buffer, int i) {
+
+
+
+	public int findNextAlignment(byte[] buffer) {
+		// Minimum length Polar packets is 8, so stop search 8 bytes before buffer ends.
+		for (int i = 0; i < buffer.length - 8; i++) {
+			if (packetValid(buffer,i)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	private boolean packetValid (byte[] buffer, int i) {
 		boolean headerValid = (buffer[i] & 0xFF) == 0xFE;
 		boolean checkbyteValid = (buffer[i + 2] & 0xFF) == (0xFF - (buffer[i + 1] & 0xFF));
 		boolean sequenceValid = (buffer[i + 3] & 0xFF) < 16;
 
 		return headerValid && checkbyteValid && sequenceValid;
 	}
+
+
+
 }
